445a2b0c992c2e809990c0c0ec3d047377304a32
juste frank <justefrank9@gmail.com>
2026-03-21 21:54:30 +0000

    #1 configuration du projet

---
+++ src/test/java/com/webgram/AppAnnuaireSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAnnuaireSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
